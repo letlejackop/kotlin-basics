6747cb3b1dd6b8788693ff5d0484405bc7740684
letlejackop <92296357+letlejackop@users.noreply.github.com>
2023-01-01 18:46:12 +0300

    kotlin types basics exercise

---
+++ basics.kt
@@ -0,0 +1,15 @@
+package com.example.myfirstapp
+
+import kotlin.reflect.typeOf
+
+fun main() {
+    var name: String="Abdullah"
+    var fNumber:Float = 20.6F
+    var dNumber:Double = 8.9746325
+    var bNumber:Byte = 25
+    var sNumber:Short = 2022
+    var number:Long = 321984526954
+    var bValue:Boolean = true
+    var cValue:Char = 'a'
+
+}
