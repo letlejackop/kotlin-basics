37dd629e183ea06f3d63100fc08a4dcabfd94161
letlejackop <92296357+letlejackop@users.noreply.github.com>
2023-01-02 19:33:10 +0300

    assignment 2 loops

---
+++ assignment2_ex1.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstapp
+
+fun main() {
+ for (i in 1..100){
+     println(i)
+     if(i==71){
+         println("IT'S OVER 70")
+         break
+     }
+ }
+}
---
+++ assignment2_ex2.kt
@@ -0,0 +1,14 @@
+package com.example.myfirstapp
+
+fun main() {
+    var temp = 35
+    while (temp==10){
+        when(temp){
+            30-> println("it's Hot")
+            20-> println("it's Comfy")
+            15-> println("its Cold")
+            else-> println(temp)
+        }
+        temp--
+    }
+}
