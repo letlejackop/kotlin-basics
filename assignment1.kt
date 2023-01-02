3cfcb8667e1f89b803e5d76575b5a301ffea31c1
letlejackop <92296357+letlejackop@users.noreply.github.com>
2023-01-02 19:46:53 +0300

    assignment 1 operators

---
+++ assignment1.kt
@@ -0,0 +1,19 @@
+package com.example.myfirstapp
+
+fun main() {
+    false&&false
+    // false
+    10%3
+    //1
+    5>=5
+    //true
+    false||true
+    //true
+    5-3*2
+    //-1
+    3!=2
+    //true
+    (6+6)/3
+    //4
+
+}
