c1380bb8920b801d02bc7f02cfe35bb4dff3b108
letlejackop <92296357+letlejackop@users.noreply.github.com>
2023-01-02 19:35:28 +0300

    Update assignment2_ex2.kt

--- assignment2_ex2.kt
+++ assignment2_ex2.kt
@@ -2,7 +2,7 @@ package com.example.myfirstapp
 
 fun main() {
     var temp = 35
-    while (temp==10){
+    while (temp>10){
         when(temp){
             30-> println("it's Hot")
             20-> println("it's Comfy")
