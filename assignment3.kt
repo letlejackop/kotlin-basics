04ce82ecaedaaf5b8acb24000c085138dbccb344
letlejackop <92296357+letlejackop@users.noreply.github.com>
2023-01-03 18:07:28 +0300

    Update assignment3.kt

--- assignment3.kt
+++ assignment3.kt
@@ -1,5 +1,15 @@
 package com.example.myfirstapp
 
 fun main() {
-
+    var n = arrayListOf<Double>()
+    n.add(1.1)
+    n.add(2.1)
+    n.add(3.1)
+    n.add(4.1)
+    n.add(5.1)
+    var sum=0.0
+    for (num in n){
+        sum+=num
+    }
+    print(sum/n.size)
 }
